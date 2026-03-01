b38dfb28a0e5c5afa01b9a48a25d8207642d40ce
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2026-03-02 00:23:27 +0300

    Header ve slider partialların tamamlanması

---
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultBoxBarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultBoxBarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultSliderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

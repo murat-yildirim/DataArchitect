192f70176a2cccb6806d58a30060bdc470268b2a
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2026-03-02 23:14:48 +0300

    About - Offer - GetInTouch ve How It Works alanları tamamlandı

---
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultBrandComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultBrandComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+            
+    }
+}
---
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultOfferComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

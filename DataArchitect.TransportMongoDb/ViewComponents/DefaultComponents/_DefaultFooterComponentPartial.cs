74967a72af7ccaa202d67488acf6d32fc0afd788
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2026-03-03 22:57:59 +0300

    View Component işlemleri tamamlandı

---
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultScriptsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultScriptsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

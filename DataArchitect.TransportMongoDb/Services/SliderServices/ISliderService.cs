12a61268f22e78423eb703683100f0ad7f26d14f
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2026-03-05 22:58:55 +0300

    AppSettings güncellendi ve servisler kaydedildi

---
+++ DataArchitect.TransportMongoDb/Services/SliderServices/ISliderService.cs
@@ -0,0 +1,13 @@
+﻿using DataArchitect.TransportMongoDb.Dtos.SliderDtos;
+
+namespace DataArchitect.TransportMongoDb.Services.SliderServices
+{
+    public interface ISliderService
+    {
+        Task<List<ResultSliderDto>> GetAllSliderAsync();
+        Task CreateSliderAsync(CreateSliderDto createSliderDto);
+        Task UpdateSliderAsync(UpdateSliderDto updateSliderDto);
+        Task<GetSliderByIdDto> GetSliderByIdAsync(string id);
+        Task DeleteSliderAsync(string id);
+    }
+}
---
+++ DataArchitect.TransportMongoDb/Settings/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DataArchitect.TransportMongoDb.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string SliderCollectionName { get; set; }
+    }
+}

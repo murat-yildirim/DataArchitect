d95c1b6b9ca4e8a8699603c67d4a803b5509316f
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2026-03-06 23:29:12 +0300

    Admin panelden eklenen sliderların ana sayfada gösterimi eklendi

--- DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ DataArchitect.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataArchitect.TransportMongoDb.Services.SliderServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DataArchitect.TransportMongoDb.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly ISliderService _sliderService;
+
+        public _DefaultSliderComponentPartial(ISliderService sliderService)
         {
-            return View();
+            _sliderService = sliderService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _sliderService.GetAllSliderAsync();
+            return View(values);
         }
     }
 }
